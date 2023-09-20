f4b3554859665eab6c78a5fed05926d02620ec74
Jhone Rodrigues <jhonecosta30@gmail.com>
2023-09-20 11:08:33 -0300

    Added Insert and Update method

--- src/main/java/com/cardapio/controller/FoodController.java
+++ src/main/java/com/cardapio/controller/FoodController.java
@@ -5,14 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cardapio.DTO.FoodRequestDTO;
 import com.cardapio.DTO.FoodResponseDTO;
+import com.cardapio.entity.Food;
 import com.cardapio.service.FoodService;
 
 @RestController
@@ -33,5 +37,17 @@ public class FoodController {
 	@PostMapping
 	public void insert(@RequestBody FoodRequestDTO data){
 		service.insert(data);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Food> update(@PathVariable Long id, @RequestBody FoodRequestDTO obj){
+		Food entity = service.update(id, obj);
+		return ResponseEntity.ok().body(entity);
 	}
 }
--- src/main/java/com/cardapio/service/FoodService.java
+++ src/main/java/com/cardapio/service/FoodService.java
@@ -3,12 +3,18 @@ package com.cardapio.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.cardapio.DTO.FoodRequestDTO;
 import com.cardapio.entity.Food;
+import com.cardapio.exception.DatabaseException;
+import com.cardapio.exception.ResourceNotFoundException;
 import com.cardapio.repository.FoodRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class FoodService {
@@ -23,5 +29,31 @@ public class FoodService {
 	public void insert(@RequestBody FoodRequestDTO data) {
 		Food foodData = new Food(data);
 		repository.save(foodData);
+	}
+	
+	public void delete (Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Food update(Long id, FoodRequestDTO obj) {
+		try {
+			Food entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	public void updateData(Food entity, FoodRequestDTO obj){
+		entity.setImage(obj.image());
+		entity.setPrice(obj.price());
+		entity.setTitle(obj.title());
 	}
 }
